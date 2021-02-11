c4ad34001c1e3c6d2c472c118aea51b1ef2cdf4a
jua <you@example.com>
2021-02-11 14:33:35 +0900

    vue_todoList_0211

--- vue/vue-todo/src/components/TodoInput.vue
+++ vue/vue-todo/src/components/TodoInput.vue
@@ -17,11 +17,14 @@ export default {
   },
   methods: {
     addTodo: function() {
-      console.log(this.newTodoItem);
-      // 저장하는 로직
-      localStorage.setItem(this.newTodoItem, this.newTodoItem);
-      this.clearInput();
-    },
+      if (this.newTodoItem !== '') {
+        var obj = {completed: false, item: this.newTodoItem};
+        // 저장하는 로직
+        // JS 객체를 String으로 변환해주는 API
+        localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+        this.clearInput();
+        }
+      },
     clearInput: function() {
       this.newTodoItem = '';
     }
--- vue/vue-todo/src/components/TodoList.vue
+++ vue/vue-todo/src/components/TodoList.vue
@@ -2,8 +2,11 @@
   <div>
     <!-- ul>li*3 -->
     <ul>
-      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
-        {{ todoItem }}
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+        <!-- todoItem이 completed일 경우: 활성화 -->
+        <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}"
+            v-on:click="toggleComplete(todoItem, index)"></i>
+        <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>        
         <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
             <i class="far fa-trash-alt"></i>
         </span>    
@@ -22,13 +25,27 @@ export default {
   methods: {
     removeTodo: function(todoItem, index) {
       console.log(todoItem, index);
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    },
+    toggleComplete: function(todoItem, index) {
+      console.log(index)
+      // console.log(todoItem, index);
+      todoItem.completed = !todoItem.completed;
+      // 로컬 스토리지의 데이터를 갱신
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
     }
   },
   created: function() {
     if (localStorage.length > 0) {
       for (var i = 0; i < localStorage.length; i ++) {
         if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
-          this.todoItems.push(localStorage.key(i));
+          // this.todoItems.push(localStorage.key(i));
+          // console.log(JSON.parse(localStorage.getItem(localStorage.key(i))));
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+          // console.log(localStorage.getItem(localStorage.key(i)));
+          
           }
         // console.log(localStorage.key(i));
       }
@@ -37,7 +54,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 ul {
   list-style-type: none;
   padding-left: 0px;
@@ -62,7 +79,7 @@ li {
 }
 
 .checkBtn {
-  line-height: 45px;
+  line-height: 50px;
   color: #62acde;
   margin-right: 5px;
 }
