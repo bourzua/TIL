105b10518bf934ac09b21c26f99ad07d87b6d3a9
jua <bourzua@naver.com>
2021-07-17 23:08:12 +0900

    ArrayList

---
+++ java/src/main/java/com/example/java/day3/Array/ArrayList/Subject.java
@@ -0,0 +1,23 @@
+package com.example.java.day3.Array.ArrayList;
+
+public class Subject {
+
+    private String name;
+    private int scorePoint;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScorePoint() {
+        return scorePoint;
+    }
+
+    public void setScorePoint(int scorePoint) {
+        this.scorePoint = scorePoint;
+    }
+}
