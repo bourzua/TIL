9f8a7c44c8c2d369180ef362f757589277ce4612
jua <bourzua@naver.com>
2021-07-17 17:26:35 +0900

    Array

---
+++ java/src/main/java/com/example/java/day3/Array/ArrayTest2.java
@@ -0,0 +1,20 @@
+package com.example.java.day3.Array;
+
+public class ArrayTest2 {
+    public static void main(String[] args) {
+
+        double[] num = new double[5];
+        int size = 0;
+
+        num[0] = 10.0;  size++;
+        num[1] = 20.0;  size++;
+        num[2] = 30.0;  size++;
+
+        double total = 1;
+        for (int i = 0; i < size; i++) {
+            total *= num[i];
+        }
+        System.out.println(total);
+
+    }
+}
