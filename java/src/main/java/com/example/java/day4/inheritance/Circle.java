e6257dbdf026c0846afa49bad8b56fb4d29601a0
jua <bourzua@naver.com>
2021-07-18 15:41:03 +0900

    Inheritance

---
+++ java/src/main/java/com/example/java/day4/inheritance/Circle.java
@@ -0,0 +1,15 @@
+package com.example.java.day4.inheritance;
+
+public class Circle{
+
+    // 합성
+    Point point; // has - a
+
+    private int radius;
+
+    public Circle() {
+        point = new Point();
+    }
+
+
+}
---
+++ java/src/main/java/com/example/java/day4/inheritance/CustomerTest1.java
@@ -0,0 +1,20 @@
+package com.example.java.day4.inheritance;
+
+public class CustomerTest1 {
+
+    public static void main(String[] args) {
+
+//        Customer customerLee = new Customer();
+//        customerLee.setCustomerID(10100);
+//        customerLee.setCustomerName("Lee");
+
+        VIPCustomer customerKim = new VIPCustomer();
+        customerKim.setCustomerID(10101);
+        customerKim.setCustomerName("Kim");
+
+        customerKim.bonusPoint = 1000;
+
+//        System.out.println(customerLee.showCustomerInfo());
+        System.out.println(customerKim.showCustomerInfo());
+    }
+}
