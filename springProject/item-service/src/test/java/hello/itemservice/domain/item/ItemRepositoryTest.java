051aa2e220bf23ec5831925231261b977afe51c3
jua <bourzua@naver.com>
2021-08-13 22:23:10 +0900

    new

---
+++ springProject/item-service/src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -0,0 +1,28 @@
+package hello.itemservice.domain.item;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemRepositoryTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void clearStore() {
+    }
+}
---
+++ springProject/servlet/src/main/webapp/WEB-INF/views/new-form.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<%--상대경로--%>
+<form action="save" method="post">
+    username: <input type="text" name="username"/>
+    age: <input type="text" name="age"/>
+    <button type="submit">전송</button>
+
+</form>
+
+</body>
+</html>
