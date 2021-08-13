44a15279c43f47998d7cba3d57c61ac012a7057a
jua <bourzua@naver.com>
2021-08-14 00:04:19 +0900

    item-service

--- springProject/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ springProject/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -33,26 +33,47 @@ public class BasicItemController {
 
     @GetMapping("/add")
     public String addForm() {
-        return "basic/abcd";
+        return "basic/addForm";
     }
 
 //    @PostMapping("/add")
-//    public String save(@RequestParam String itemName,
-//                       @RequestParam int price,
-//                       @RequestParam Integer quantity,
-//                       Model model
-//    ) {
-//        Item item = new Item();
-//        item.setItemName(itemName);
-//        item.setPrice(price);
-//        item.setQuantity(quantity);
-//
-//        itemRepository.save(item);
-//
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model
+    ) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item) {
+        itemRepository.save(item);
 //        model.addAttribute("item", item);
-//
-//        return "basic/item";
-//    }
+
+        return "redirect:/basic/items/" + item.getId();
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item){
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
 
     // 테스트용 데이터 추가
     @PostConstruct
