2bccac87ad0d3be9df1c0706c2c0fb85bc58fc14
jua <bourzua@naver.com>
2021-08-15 18:06:18 +0900

    회원가입

---
+++ springProject/cosineSimilarity/src/main/java/com/example/cosinesimilarity/dto/LoginDto.java
@@ -0,0 +1,31 @@
+package com.example.cosinesimilarity.dto;
+
+public class LoginDto {
+
+    private String name;
+    private String password;
+
+    public LoginDto() {
+    }
+
+    public LoginDto(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
